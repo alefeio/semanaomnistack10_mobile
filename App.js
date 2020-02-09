0c80052fac8a6d72ed2d81af52638c2a1a33793b
Alexandre Feio <alefeio@gmail.com>
2020-02-09 04:00:38 -0300

    primeiro commit do projeto mobile React Native

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textWhite}>Hello, world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#069',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textWhite: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
